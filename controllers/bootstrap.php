e677e71f55fa728e7f349fc89057687eeb359f60
Chyngyz Sydykov <chyngyz6@gmail.com>
2017-02-14 17:24:05 +0600

    master testing

--- controllers/bootstrap.php
+++ controllers/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 //error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
-error_reporting( E_ALL );
+//error_reporting( E_ALL );
 define('ITEM_PER_PAGE',20);
 session_start();
 //get url slash variable
